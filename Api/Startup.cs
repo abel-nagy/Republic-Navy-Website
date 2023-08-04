c70820a370830e64cbe238e7a575e7a216b88695
Ábel Nagy <abelnagy@abelnagy.com>
2023-08-04 19:33:41 +0200

    Added DI to Api project and IdentityModelEventSource.ShowPII = true; for testing purposes

---
+++ Api/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Logging;
+
+[assembly: FunctionsStartup(typeof(Api.Startup))]
+
+namespace Api
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient();
+
+            //builder.Services.AddSingleton<IMyService>((s) => {
+            //    return new MyService();
+            //});
+
+            //builder.Services.AddSingleton<ILoggerProvider, MyLoggerProvider>();
+            IdentityModelEventSource.ShowPII = true;
+        }
+    }
+}
